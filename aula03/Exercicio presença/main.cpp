d1c963518dfc6b9cc0cd6e4cbe477640996c6628
andrejr95 <50457552+andrejr95@users.noreply.github.com>
2020-06-17 18:12:14 -0300

    Update main.cpp

--- aula03/Exercicio presença/main.cpp
+++ aula03/Exercicio presença/main.cpp
@@ -4,40 +4,41 @@
 
 int main(int argc, char *argv[]) {
 	
-  std::string msn;
-  std::ifstream arquivo_entrada("entrada.txt");
-  std::ofstream arquivo_saida("saida.txt", std::ios::app);
+	  std::string msn;
+	  std::ifstream arquivo_entrada("entrada.txt");
+	  std::ofstream arquivo_saida("saida.txt", std::ios::app);
 
-  if (argc <= 1) {
-    std::cout << "Uso: " << argv[0] << " add <mensagem>";
-    return 1;
-  }
- 
-  if (argc == 2 && std::string(argv[1]) == "add"){ 
+	  if (argc <= 1) {
+	    std::cout << "Uso: " << argv[0] << " add <mensagem>";
+	    return 1;
+		}
 
-      std::cout << "Digite uma mensagem: ";
-      std::getline(std::cin, msn);
-      std::cout << msn << std::endl;
-    
-  }
+	  if (argc == 2 && std::string(argv[1]) == "add"){ 
 
-  else if (argc == 3 && std::string(argv[1]) == "add") {
-		
-      std::cout << "mensagem adicionada: " << argv[2] << std::endl;
-    
-  }
+	      std::cout << "Digite uma mensagem: ";
+	      std::getline(std::cin, msn);
+	      std::cout << msn << std::endl;
 
-	 if(arquivo_saida.fail()){
+	  }
 
-        std::cout << "Arquivo não localizado" << std::endl;
+	  else if (argc == 3 && std::string(argv[1]) == "add") {
 
-        return -1;
- }
+	      std::cout << "mensagem adicionada: " << argv[2] << std::endl;
 
-	arquivo_saida << msn << std::endl;
-	arquivo_saida.close();
-  
-	std::cout << "mensagem adicionada com sucesso" << std::endl;
+	  }
+
+
+	   if(arquivo_saida.fail()){
+
+		std::cout << "Arquivo não localizado" << std::endl;
+
+		return -1;
+	 }
+
+		arquivo_saida << msn << std::endl;
+		arquivo_saida.close();
+
+		std::cout << "mensagem adicionada com sucesso" << std::endl;
 
 
   return 0;
