b1900bf8778bf95eae75548cb8ff8b83c2447522
andrejr95 <50457552+andrejr95@users.noreply.github.com>
2020-06-22 19:09:16 -0300

    Add files via upload

---
+++ aula05/Datahora.h
@@ -0,0 +1,27 @@
+#ifndef DATAHORA_H
+#define DATAHORA_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+struct Data{
+
+	int dia;
+	int mes;
+	int ano;
+
+};
+
+struct Hora{
+
+	int hora;
+	int minuto;
+	int segundo;
+
+};
+
+
+
+#endif
---
+++ aula05/mensagem.h
@@ -0,0 +1,15 @@
+#ifndef MENSAGEM_H
+#define MENSAGEM_H
+
+#include "Tempo.h"
+
+struct Mensagem {
+
+    Data data;
+    Hora hora;
+
+    void add(std::string arquivo_saida, std::string msninput);
+    void list(std::string arquivo_saida);
+};
+
+#endif
